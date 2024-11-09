6a51ab478e870306e1373af6fbaa2afc9615d23b
AFRAH-AIML <231501008@rajalakshmi.edu.in>
2024-11-09 16:59:39 +0530

    Create LoginSignup.java

---
+++ LoginSignup.java
@@ -0,0 +1,18 @@
+package loginsignup;
+
+
+public class Loginsignup {
+
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+        login loginframe=new login();
+        loginframe.setVisible(true);
+        loginframe.pack();
+        loginframe.setLocationRelativeTo(null);
+        
+        
+        
+    }
+    
+}
